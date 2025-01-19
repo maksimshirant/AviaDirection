7cacf6e143c82cd445658754a591faf4e2995fcb
Shirant Maksim <shirant.maksim@yandex.ru>
2025-01-19 11:31:21 +0300

    end

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]__[hash:base64:5]'
+    }
+  }
 })
